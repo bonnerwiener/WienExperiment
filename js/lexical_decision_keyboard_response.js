bc99a3603df105073eb280102572034d9866e665
Jeronim Morina <plattenschieber@gmail.com>
2014-04-25 18:59:02 +0200

    rearrange for better readability

--- js/lexical_decision_keyboard_response.js
+++ js/lexical_decision_keyboard_response.js
@@ -1,18 +1,20 @@
-/*
- * Use the code on this page as a template for your own experiment
-*/
-
-// Settings you can vary
-var ITI = 500 // Inter Trial Interval
+// Inter Trial Interval
+var ITI = 500 
 var number_of_trials = 40
-var user_data_address = '../save_UserData.php' // Page to send results to.
-var data_address = '../save_data.php' // Page to send results to.
+
+// pages to send results to
+var user_data_address = '../save_UserData.php' 
+var data_address = '../save_data.php' 
+
+// hidden box with forms which gets filled with data
 var logging_box_ids = new Array('paradigmBox', 'timeBox', 'subjectBox', 'trialBox', 'stimuliBox', 'probeBox', 'codeBox', 'responseBox', 'rtBox');
 var variables_to_log = new Array('paradigm', 'experiment_start_time', 'subject_nr', 'trial_number', 'stimuli_number', 'probe', 'code', 'response', 'rt');
+
+// DEBUG stuff
 var debug_mode = true // Set as true to show extra information, false to run experiment normally.
 var error_feedback = false
 
-// Variables to log
+// variables to log
 var paradigm = 'press_key';
 var experiment_start_time = Date();
 var subject_nr = Math.floor(Math.random() * 99999999) + 1; // Pick a random number between 1 and 99999999
